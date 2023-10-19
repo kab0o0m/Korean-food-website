319848836a695b4eb66b2475e04f58406563722c
yuno-cchi <fakeymcnoexist@gmail.com>
2023-10-19 18:51:06 +0800

    start routing

---
+++ fresheasy/src/lib/Header.svelte
@@ -0,0 +1,11 @@
+<div id="main-header">
+    this is a test!
+</div>
+
+<style lang="scss">
+    @import './../scss/colors.scss';
+    #main-header {
+        background-color: $background-primary;
+        width: 100vw;
+    }
+</style>
