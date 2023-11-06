89781f040859b99809021467431a34a291c52e77
yuno-cchi <fakeymcnoexist@gmail.com>
2023-11-06 22:07:08 +0800

    finally finish home page

--- fresheasy_v2/src/lib/hero.svelte
+++ fresheasy_v2/src/lib/hero.svelte
@@ -1,6 +1,6 @@
 <div class="bg-[url('./assets/banner_background.png')] w-full flex pt-16 h-4/5">
     <div class="text-center md:w-3/5 w-full md:pl-16 pb-16 mt-24">
-        <h1 class="h1 pb-4 pt-12"><b>The Best Value Meal Kit</b></h1>
+        <h1 class="h1 pb-4 md:pt-12"><b>The Best Value Meal Kit</b></h1>
         <h3 class="h3 pb-8">Discover the flavour of Seoul at your doorstep!</h3>
         <a href="./mealkits"><div class="btn bg-primary-500 text-black hover:scale-105">ORDER NOW</div></a>
     </div>
--- fresheasy_v2/src/routes/+page.svelte
+++ fresheasy_v2/src/routes/+page.svelte
@@ -4,6 +4,7 @@
 	import HeaderSpacer from '$lib/header_spacer.svelte';
 	import Footer from '$lib/footer.svelte';
 	import Infocard from '$lib/infocard.svelte';
+	import ProductSelector from '$lib/product_selector.svelte';
 </script>
 
 <main>
@@ -12,14 +13,14 @@
 <div class="flex justify-center align-middle">
 	<Hero />
 </div>
-<div class="container mx-auto flex justify-center items-center">
-	<div class="bg-white w-full py-8">
+<div class="mx-auto flex justify-center items-center">
+	<div class="bg-white w-screen py-8">
 		<div class="text-center">
 			<h2 class="h2 font-bold py-4">
 				How it works
 			</h2>
 		</div>
-		<div class="flex justify-center content-center space-x-8 pt-8">
+		<div class="flex flex-wrap lg:flex-nowrap justify-center content-center py-8 bg-white w-full">
 			<Infocard 
 				icon='<svg xmlns="http://www.w3.org/2000/svg" height="4em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M176 56c0-13.3 10.7-24 24-24h16c13.3 0 24 10.7 24 24s-10.7 24-24 24H200c-13.3 0-24-10.7-24-24zm24 48h16c13.3 0 24 10.7 24 24s-10.7 24-24 24H200c-13.3 0-24-10.7-24-24s10.7-24 24-24zM56 176H72c13.3 0 24 10.7 24 24s-10.7 24-24 24H56c-13.3 0-24-10.7-24-24s10.7-24 24-24zM0 283.4C0 268.3 12.3 256 27.4 256H484.6c15.1 0 27.4 12.3 27.4 27.4c0 70.5-44.4 130.7-106.7 154.1L403.5 452c-2 16-15.6 28-31.8 28H140.2c-16.1 0-29.8-12-31.8-28l-1.8-14.4C44.4 414.1 0 353.9 0 283.4zM224 200c0-13.3 10.7-24 24-24h16c13.3 0 24 10.7 24 24s-10.7 24-24 24H248c-13.3 0-24-10.7-24-24zm-96 0c0-13.3 10.7-24 24-24h16c13.3 0 24 10.7 24 24s-10.7 24-24 24H152c-13.3 0-24-10.7-24-24zm-24-96h16c13.3 0 24 10.7 24 24s-10.7 24-24 24H104c-13.3 0-24-10.7-24-24s10.7-24 24-24zm216 96c0-13.3 10.7-24 24-24h16c13.3 0 24 10.7 24 24s-10.7 24-24 24H344c-13.3 0-24-10.7-24-24zm-24-96h16c13.3 0 24 10.7 24 24s-10.7 24-24 24H296c-13.3 0-24-10.7-24-24s10.7-24 24-24zm120 96c0-13.3 10.7-24 24-24h16c13.3 0 24 10.7 24 24s-10.7 24-24 24H440c-13.3 0-24-10.7-24-24zm-24-96h16c13.3 0 24 10.7 24 24s-10.7 24-24 24H392c-13.3 0-24-10.7-24-24s10.7-24 24-24zM296 32h16c13.3 0 24 10.7 24 24s-10.7 24-24 24H296c-13.3 0-24-10.7-24-24s10.7-24 24-24z"/></svg>'
 				number="1"
@@ -44,6 +45,76 @@
 				title="Eat it!"
 				text="Enjoy your delicious meal!"
 			/>
+		</div>
+		<div class="bg-[url('./assets/banner_background.png')] w-full h-4/5 py-12">
+			<div class="flex justify-center content-center pb-12">
+				<h2 class="h2 font-bold text-center">Why us?</h2>
+			</div>
+			<div class="">
+				<ul class="px-12 sm:px-24 md:px-36 lg:px-48 xl:px-96">
+					<li class="py-6">
+						<div class="flex">
+							<div class="md:w-3/12 flex float-right justify-center pl-6">
+								<svg xmlns="http://www.w3.org/2000/svg" height="6em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/></svg>
+							</div>
+							<div class="w-8/12 md:w-9/12 pl-2">
+								<h4 class="h4 font-bold pt-4">Save Time</h4>
+								<p class="">Skip trips to the supermarket</p>
+							</div>
+						</div>
+					</li>
+					<li class="py-6">
+						<div class="flex">
+							<div class="md:w-3/12 flex float-right justify-center pl-6">
+								<svg xmlns="http://www.w3.org/2000/svg" height="6em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M320 96H192L144.6 24.9C137.5 14.2 145.1 0 157.9 0H354.1c12.8 0 20.4 14.2 13.3 24.9L320 96zM192 128H320c3.8 2.5 8.1 5.3 13 8.4C389.7 172.7 512 250.9 512 416c0 53-43 96-96 96H96c-53 0-96-43-96-96C0 250.9 122.3 172.7 179 136.4l0 0 0 0c4.8-3.1 9.2-5.9 13-8.4zm84 88c0-11-9-20-20-20s-20 9-20 20v14c-7.6 1.7-15.2 4.4-22.2 8.5c-13.9 8.3-25.9 22.8-25.8 43.9c.1 20.3 12 33.1 24.7 40.7c11 6.6 24.7 10.8 35.6 14l1.7 .5c12.6 3.8 21.8 6.8 28 10.7c5.1 3.2 5.8 5.4 5.9 8.2c.1 5-1.8 8-5.9 10.5c-5 3.1-12.9 5-21.4 4.7c-11.1-.4-21.5-3.9-35.1-8.5c-2.3-.8-4.7-1.6-7.2-2.4c-10.5-3.5-21.8 2.2-25.3 12.6s2.2 21.8 12.6 25.3c1.9 .6 4 1.3 6.1 2.1l0 0 0 0c8.3 2.9 17.9 6.2 28.2 8.4V424c0 11 9 20 20 20s20-9 20-20V410.2c8-1.7 16-4.5 23.2-9c14.3-8.9 25.1-24.1 24.8-45c-.3-20.3-11.7-33.4-24.6-41.6c-11.5-7.2-25.9-11.6-37.1-15l0 0-.7-.2c-12.8-3.9-21.9-6.7-28.3-10.5c-5.2-3.1-5.3-4.9-5.3-6.7c0-3.7 1.4-6.5 6.2-9.3c5.4-3.2 13.6-5.1 21.5-5c9.6 .1 20.2 2.2 31.2 5.2c10.7 2.8 21.6-3.5 24.5-14.2s-3.5-21.6-14.2-24.5c-6.5-1.7-13.7-3.4-21.1-4.7V216z"/></svg>
+							</div>
+							<div class="w-8/12 md:w-9/12 pl-2">
+								<h4 class="h4 font-bold pt-4">Save Money</h4>
+								<p class="">Enjoy tasty and affordable meals, at less than $10 per meal</p>
+							</div>
+						</div>
+					</li>
+					<li class="py-6">
+						<div class="flex">
+								<div class="md:w-3/12 flex float-right justify-center pl-6">
+									<svg xmlns="http://www.w3.org/2000/svg" height="6em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M240 144A96 96 0 1 0 48 144a96 96 0 1 0 192 0zm44.4 32C269.9 240.1 212.5 288 144 288C64.5 288 0 223.5 0 144S64.5 0 144 0c68.5 0 125.9 47.9 140.4 112h71.8c8.8-9.8 21.6-16 35.8-16H496c26.5 0 48 21.5 48 48s-21.5 48-48 48H392c-14.2 0-27-6.2-35.8-16H284.4zM144 80a64 64 0 1 1 0 128 64 64 0 1 1 0-128zM400 240c13.3 0 24 10.7 24 24v8h96c13.3 0 24 10.7 24 24s-10.7 24-24 24H280c-13.3 0-24-10.7-24-24s10.7-24 24-24h96v-8c0-13.3 10.7-24 24-24zM288 464V352H512V464c0 26.5-21.5 48-48 48H336c-26.5 0-48-21.5-48-48zM48 320h80 16 32c26.5 0 48 21.5 48 48s-21.5 48-48 48H160c0 17.7-14.3 32-32 32H64c-17.7 0-32-14.3-32-32V336c0-8.8 7.2-16 16-16zm128 64c8.8 0 16-7.2 16-16s-7.2-16-16-16H160v32h16zM24 464H200c13.3 0 24 10.7 24 24s-10.7 24-24 24H24c-13.3 0-24-10.7-24-24s10.7-24 24-24z"/></svg>
+								</div>
+								<div class="w-8/12 md:w-9/12 pl-2">
+									<h4 class="h4 font-bold pt-4">Easy to Cook</h4>
+									<p class="">Our meal kit recipes have less than 5 steps to follow</p>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<h2 class="h2 text-center pt-8 font-bold">Our Meal Kits</h2>
+		<div class="flex flex-wrap lg:flex-nowrap justify-center text-center content-center py-8 bg-white w-full">
+			<ProductSelector
+				product_image='<img alt="" src="./assets/beef_bulgogi_img.png" />'
+				product_name='Beef Bulgogi'
+				product_price='6.00'
+			/>
+			<ProductSelector
+				product_image='<img alt="" src="./assets/spicy_chicken_bulgogi_img.png" />'
+				product_name='Spicy Chicken Bulgogi'
+				product_price='6.00'
+			/>
+			<ProductSelector
+				product_image='<img alt="" src="./assets/spicy_pork_bulgogi_img.png" />'
+				product_name='Spicy Pork Bulgogi'
+				product_price='6.00'
+			/>
+			<ProductSelector
+				product_image='<img alt="" src="./assets/army_stew_img.png" />'
+				product_name='Army Stew'
+				product_price='6.00'
+			/>
+		</div>
+		<div class="flex justify-center">
+			<a href="checkout"><div class="w-48 rounded-lg bg-primary-500 hover:bg-primary-600 text-center h-12 pt-3 text-white font-bold">
+				CHECKOUT NOW
+			</div></a>
 		</div>
 	</div>
 </div>
